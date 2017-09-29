b81ef7c51fbfee3a1b961675dda258d01d4242c5
Happiness Nneke <happymba11@yahoo.com>
2017-09-29 09:58:16 +0000

    Deployment

--- js/directive.js
+++ js/directive.js
@@ -2,6 +2,6 @@ angular.module("TodoDirective",[])
    .directive("todoTable", function(){
        return{
            restrict: "A",
-           templateUrl: "templates/directive/todo-table.html"
+           templateUrl: "templates/directives/todo-table.html"
        };
    });
